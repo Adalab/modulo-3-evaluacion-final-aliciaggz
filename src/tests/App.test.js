b7a83f28aefc2f174c578d3df82fc4a0fc7659d3
Alicia Gallego Gonzalez <aliciagallegonz@gmail.com>
2023-03-15 16:39:12 +0100

    update test passed

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -2,12 +2,16 @@ import { render, screen } from '@testing-library/react';
 import App from '../components/App';
 
 describe('Harry Potter App', () => {
-  it('should show a list of characters from the API', () => {
-    window.fetch.mockResolvedValueOnce({ ok: true });
+  beforeAll(() => jest.spyOn(window, 'fetch'));
+  it('should show a list of characters from the API', async () => {
+    window.fetch.mockResolvedValueOnce({
+      ok: true,
+      json: async () => ({ name: 'Harry Potter' }),
+    });
+    render(<App />);
+    expect(window.fetch).toHaveBeenCalledTimes(1);
+    expect(window.fetch).toHaveBeenCalledWith(
+      'https://hp-api.onrender.com/api/characters/house/gryffindor'
+    );
   });
-  render(<App />);
-  expect(window.fetch).toHaveBeenCalledTimes(1);
-  expect(window.fetch).toHaveBeenCalledWith(
-    'https://hp-api.onrender.com/api/characters/house'
-  );
 });
